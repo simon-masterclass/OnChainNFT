b59949ece025ed44beea297e3fdaf35928d4a904
Simon Ionashku <simon.ionashku@gmail.com>
2023-05-31 21:05:49 -0400

    [04] - OnChainNFT contract

--- contracts/OnChainNFT.sol
+++ contracts/OnChainNFT.sol
@@ -1,49 +1,25 @@
 // SPDX-License-Identifier: MIT
 
-// Amended by HashLips
-/**
-    !Disclaimer!
-    These contracts have been used to create tutorials,
-    and was created for the purpose to teach people
-    how to create smart contracts on the blockchain.
-    please review this code on your own before using any of
-    the following code for production.
-    HashLips will not be liable in any way if for the use 
-    of the code. That being said, the code has been tested 
-    to the best of the developers' knowledge to work as intended.
-*/
+// Amended by @simon-masterclass from the HashLips Art Engine
 
 pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Base64.sol";
 
 contract OnChainNFT is ERC721Enumerable, Ownable {
     using Strings for uint256;
 
-    string baseURI;
-    string public baseExtension = ".json";
     uint256 public cost = 0.05 ether;
     uint256 public maxSupply = 10000;
     uint256 public maxMintAmount = 20;
     bool public paused = false;
-    bool public revealed = false;
-    string public notRevealedUri;
 
     constructor(
         string memory _name,
-        string memory _symbol,
-        string memory _initBaseURI,
-        string memory _initNotRevealedUri
-    ) ERC721(_name, _symbol) {
-        setBaseURI(_initBaseURI);
-        setNotRevealedURI(_initNotRevealedUri);
-    }
-
-    // internal
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseURI;
-    }
+        string memory _symbol
+    ) ERC721(_name, _symbol) {}
 
     // public
     function mint(uint256 _mintAmount) public payable {
@@ -80,49 +56,15 @@ contract OnChainNFT is ERC721Enumerable, Ownable {
             _exists(tokenId),
             "ERC721Metadata: URI query for nonexistent token"
         );
-
-        if (revealed == false) {
-            return notRevealedUri;
-        }
-
-        string memory currentBaseURI = _baseURI();
-        return
-            bytes(currentBaseURI).length > 0
-                ? string(
-                    abi.encodePacked(
-                        currentBaseURI,
-                        tokenId.toString(),
-                        baseExtension
-                    )
-                )
-                : "";
     }
 
     //only owner
-    function reveal() public onlyOwner {
-        revealed = true;
-    }
-
     function setCost(uint256 _newCost) public onlyOwner {
         cost = _newCost;
     }
 
     function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
         maxMintAmount = _newmaxMintAmount;
-    }
-
-    function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
-        notRevealedUri = _notRevealedURI;
-    }
-
-    function setBaseURI(string memory _newBaseURI) public onlyOwner {
-        baseURI = _newBaseURI;
-    }
-
-    function setBaseExtension(
-        string memory _newBaseExtension
-    ) public onlyOwner {
-        baseExtension = _newBaseExtension;
     }
 
     function pause(bool _state) public onlyOwner {
